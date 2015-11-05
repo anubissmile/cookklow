16ee52fd8c09cdb7d5de5b9ca2793eeeb88f1aba
Anubisunpluged <anubis.smile@gmail.com>
2015-11-06 01:03:37 +0700

    SAVE
    SAVE

--- resources/views/metro-dashboard/home.blade.php
+++ resources/views/metro-dashboard/home.blade.php
@@ -23,12 +23,8 @@
 	@include('metro-dashboard.script.scriptheader')
 	@yield('javascript')
 	<!-- END-HEADER-JAVASCRIPT -->
-	
-		
-		
-		
-</head>
 
+</head>
 <body>
 		<!-- start: Header -->
 		@include('metro-dashboard.section.header')
@@ -37,36 +33,9 @@
 		<div class="container-fluid-full">
 		<div class="row-fluid">
 				
-			<!-- start: Main Menu -->
-			<div id="sidebar-left" class="span2">
-				<div class="nav-collapse sidebar-nav">
-					<ul class="nav nav-tabs nav-stacked main-menu">
-						<li><a href="index.html"><i class="icon-bar-chart"></i><span class="hidden-tablet"> Dashboard</span></a></li>	
-						<li><a href="messages.html"><i class="icon-envelope"></i><span class="hidden-tablet"> Messages</span></a></li>
-						<li><a href="tasks.html"><i class="icon-tasks"></i><span class="hidden-tablet"> Tasks</span></a></li>
-						<li><a href="ui.html"><i class="icon-eye-open"></i><span class="hidden-tablet"> UI Features</span></a></li>
-						<li><a href="widgets.html"><i class="icon-dashboard"></i><span class="hidden-tablet"> Widgets</span></a></li>
-						<li>
-							<a class="dropmenu" href="#"><i class="icon-folder-close-alt"></i><span class="hidden-tablet"> Dropdown</span><span class="label label-important"> 3 </span></a>
-							<ul>
-								<li><a class="submenu" href="submenu.html"><i class="icon-file-alt"></i><span class="hidden-tablet"> Sub Menu 1</span></a></li>
-								<li><a class="submenu" href="submenu2.html"><i class="icon-file-alt"></i><span class="hidden-tablet"> Sub Menu 2</span></a></li>
-								<li><a class="submenu" href="submenu3.html"><i class="icon-file-alt"></i><span class="hidden-tablet"> Sub Menu 3</span></a></li>
-							</ul>	
-						</li>
-						<li><a href="form.html"><i class="icon-edit"></i><span class="hidden-tablet"> Forms</span></a></li>
-						<li><a href="chart.html"><i class="icon-list-alt"></i><span class="hidden-tablet"> Charts</span></a></li>
-						<li><a href="typography.html"><i class="icon-font"></i><span class="hidden-tablet"> Typography</span></a></li>
-						<li><a href="gallery.html"><i class="icon-picture"></i><span class="hidden-tablet"> Gallery</span></a></li>
-						<li><a href="table.html"><i class="icon-align-justify"></i><span class="hidden-tablet"> Tables</span></a></li>
-						<li><a href="calendar.html"><i class="icon-calendar"></i><span class="hidden-tablet"> Calendar</span></a></li>
-						<li><a href="file-manager.html"><i class="icon-folder-open"></i><span class="hidden-tablet"> File Manager</span></a></li>
-						<li><a href="icon.html"><i class="icon-star"></i><span class="hidden-tablet"> Icons</span></a></li>
-						<li><a href="login.html"><i class="icon-lock"></i><span class="hidden-tablet"> Login Page</span></a></li>
-					</ul>
-				</div>
-			</div>
-			<!-- end: Main Menu -->
+			<!-- START-SIDEBAR-LEFT-MAINMENU -->
+			@include('metro-dashboard.section.sidebar-left')
+			<!-- END-SIDEBAR-LEFT-MAINMENU -->
 			
 			<noscript>
 				<div class="alert alert-block span10">
@@ -79,14 +48,10 @@
 			<div id="content" class="span10">
 			
 			
-			<ul class="breadcrumb">
-				<li>
-					<i class="icon-home"></i>
-					<a href="index.html">Home</a> 
-					<i class="icon-angle-right"></i>
-				</li>
-				<li><a href="#">Dashboard</a></li>
-			</ul>
+			<!-- START-BREADCRUMB -->
+			@include('metro-dashboard.section.breadcrumb')
+			<!-- END-BREADCRUMB -->
+
 
 			<div class="row-fluid">
 				
@@ -738,14 +703,9 @@
 	
 	<div class="clearfix"></div>
 	
-	<footer>
-
-		<p>
-			<span style="text-align:left;float:left">&copy; 2013 <a href="http://jiji262.github.io/Bootstrap_Metro_Dashboard/" alt="Bootstrap_Metro_Dashboard">Bootstrap Metro Dashboard</a></span>
-			
-		</p>
-
-	</footer>
+	<!-- START-FOOTER -->
+	@include('metro-dashboard.section.footer')
+	<!-- END-FOOTER -->
 	
 	
 	<!-- START-FOOTER-JAVASCRIPT -->
